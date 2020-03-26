f297fe1a4ca0c3414b2efeb16067c57d4408f32f
System User <zasina@jupyter.korpus.cz>
2020-03-26 17:11:43 +0100

    Hlášení chyb

--- grep.py
+++ grep.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 #hash bang = shebang
+# docstring = dokumentační řetězec; uloží do do proměnné _doc_
+"""Usage: grep.py PATTERN FILE
+
+Print lines form FILE matching regular expression PATTERN
+
+"""
 
 import sys
 import regex as re
 
-pattern, path = sys.argv[1:]
-with open(path) as file:
+try:
+    pattern, path = sys.argv[1:]
+except ValueError:
+    print(_doc_.strip(), file=sys.stderr)
+    sys.exit(1)
+    
+try:
+    with open(path) as file:
     for line in file:
         line = line.strip("\n")
         if re.search(pattern, line):
             print(line)
+except FileNotFoundError as err:
+    print(_doc_.strip(), file=sys.stderr)
+    print(err, file=sys.stderr)
+    sys.exit(1)
