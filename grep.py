cc5b340fcdfd0fe2d80be170914351ad4dc37aae
System User <zasina@jupyter.korpus.cz>
2020-03-26 16:47:32 +0100

    Regulární výrazy

--- grep.py
+++ grep.py
@@ -2,10 +2,11 @@
 #hash bang = shebang
 
 import sys
+import regex as re
 
 pattern, path = sys.argv[1:]
 with open(path) as file:
     for line in file:
         line = line.strip("\n")
-        if pattern in line:
+        if re.search(pattern, line):
             print(line)
