b8a4ebd5a470c3e200ab815be1510886df5898b7
System User <zasina@jupyter.korpus.cz>
2020-03-26 17:25:36 +0100

    Balení do funkcí

--- grep.py
+++ grep.py
@@ -10,19 +10,28 @@ Print lines form FILE matching regular expression PATTERN
 import sys
 import regex as re
 
-try:
-    pattern, path = sys.argv[1:]
-except ValueError:
-    print(_doc_.strip(), file=sys.stderr)
-    sys.exit(1)
-    
-try:
-    with open(path) as file:
-    for line in file:
-        line = line.strip("\n")
+def grep(pattern, lines):
+    """Print lines matching pattern."""
+    for line in lines:
+        line = line.strip()
         if re.search(pattern, line):
-            print(line)
-except FileNotFoundError as err:
-    print(_doc_.strip(), file=sys.stderr)
-    print(err, file=sys.stderr)
-    sys.exit(1)
+            pritn(line)
+            
+def parse_argv(argv):
+    pattern, path = argv[1:]
+    return pattern, path
+
+def main():
+    try:
+        pattern, path = parse_argv(sys.argv)
+    except ValueError:
+        print(_doc_.strip(), file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        with open(path) as file:
+            grep(pattern, file)
+    except FileNotFoundError as err:
+        print(_doc_.strip(), file=sys.stderr)
+        print(err, file=sys.stderr)
+        sys.exit(1)
